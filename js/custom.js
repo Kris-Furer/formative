f664c8d87351864eec2cb00139f9e37593e3b32c
Kris Furer <270034121@bun2-206-10.acg.ac.nz>
2021-08-05 12:09:28 +1200

    added search

--- js/custom.js
+++ js/custom.js
@@ -3,42 +3,49 @@ document.getElementById('keyword').innerHTML = " ";
 // Dom elements::::::::::::::::::::::::
 var search = document.querySelector('#search')
 
-search.onclick= function(){
-  // User Choices::::::::::::::::::::::::::::::::::
-  var keyword = document.querySelector('#keyword').value
-  $.ajax({
-    url : 'https://newsapi.org/v2/everything?q=' + keyword + '&from=2021-07-28&to=2021-07-28&sortBy=popularity&apiKey=0ff9b09f976343aba1d3ae53a7a2cf09',
-    type: 'GET',
-    data: 'json',
-    success: function(data){
-      console.log(data);
-      var i;
-      for (i=0;i< data.articles.length;i++){
-        console.log(data.articles[i].title);
-         document.getElementById('objects').innerHTML +=
-         '<div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4"> ' +
-            '<div class="card mt-3 ml-5" style="width: 18rem;">' +
-              '<div class="card-body">'+
-                '<h5 class="card-title">'+ data.articles[i].title +'</h5>'+
-                '<img style="width:100%" src ='+ data.articles[i].urlToImage +'>' +
-               '<p class="card-text"> '
-               + data.articles[i].color+ '<br>'
-                + data.articles[i].content + '<br> ' +'width ' + data.articles[i].width+'</p>'+
+  // search input
 
-             '</div>'+
-            '</div>'+
-         '</div>';
-      } //for loop
-    },  //success
-    error : function(){
-      console.log('error');
-    } //error
-  }); //ajax
-  console.log("hi");
 
-  console.log(keyword);
-}
-
+// User Choices::::::::::::::::::::::::::::::::::
+// var keyword = ''
 
 // Choose category
+
+
+
+search.onclick = function(){
+var keyword = document.querySelector('#keyword').value
 var category = document.querySelector('#category').value
+var sortBy = document.querySelector('#sort').value
+$.ajax({
+  url : 'https://newsapi.org/v2/everything?q=' + keyword + '&from=2021-07-28&to=2021-07-28&sortBy=' + sortBy + '&apiKey=0ff9b09f976343aba1d3ae53a7a2cf09',
+  type: 'GET',
+  data: 'json',
+  success: function(data){
+    console.log(data);
+    var i;
+
+    document.getElementById('objects').innerHTML = ""
+    for (i=0;i< data.articles.length;i++){
+
+       document.getElementById('objects').innerHTML +=
+       '<div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6"> ' +
+          '<div class="card mt-3 ml-5"' +
+            '<div class="card-body ">'+
+
+              '<img class="mb-2" style="width:100%" src ='+ data.articles[i].urlToImage +'>' +
+                '<h5 class="card-title my-3 p-2 h3">'+ data.articles[i].title +'</h5>'+
+             '<p class="card-text p-2"> '
+             // + data.articles[i].color+ '<br>'
+              + data.articles[i].description + '<br> ' +'Author: ' + data.articles[i].author +'</p>'+
+
+           '</div>'+
+          '</div>'+
+       '</div>';
+    } //for loop
+  },  //success
+  error : function(){
+    console.log('error');
+  } //error
+}); //ajax
+}
