c3b1c3afe1e894638423e58b3f30f1e775640877
Kris Furer <270034121@bun2-206-10.acg.ac.nz>
2021-08-06 12:32:09 +1200

    didnt do much at all actually

--- js/custom.js
+++ js/custom.js
@@ -3,17 +3,11 @@ document.getElementById('keyword').innerHTML = " ";
 // Dom elements::::::::::::::::::::::::
 var search = document.querySelector('#search')
 
-  // search input
-
-
-// User Choices::::::::::::::::::::::::::::::::::
-// var keyword = ''
-
-// Choose category
 
 
 
 search.onclick = function(){
+
 var keyword = document.querySelector('#keyword').value
 var category = document.querySelector('#category').value
 var sortBy = document.querySelector('#sort').value
@@ -25,27 +19,35 @@ $.ajax({
     console.log(data);
     var i;
 
-    document.getElementById('objects').innerHTML = ""
+    document.getElementById('objects').innerHTML = "";
     for (i=0;i< data.articles.length;i++){
-
+    console.log(data.articles[i].source.name);
        document.getElementById('objects').innerHTML +=
        '<div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6"> ' +
           '<div class="card mt-3 ml-5"' +
             '<div class="card-body ">'+
 
-              '<img class="mb-2" style="width:100%" src ='+ data.articles[i].urlToImage +'>' +
+              '<img class="mb-2 card-img" src ='+ data.articles[i].urlToImage +'>' +
                 '<h5 class="card-title my-3 p-2 h3">'+ data.articles[i].title +'</h5>'+
              '<p class="card-text p-2"> '
              // + data.articles[i].color+ '<br>'
               + data.articles[i].description + '<br> ' +'Author: ' + data.articles[i].author +'</p>'+
+              ' <a class="btn btn-primary btn-lg btn-block m-2" href="article.html"><div>See More</div></a>' +
 
            '</div>'+
           '</div>'+
        '</div>';
+
     } //for loop
+
+
+  // var articleLink = document.querySelectorAll('.article-link')
+  // for (var i = 0; i < articleLink.length; i++) {
+  //   articleLink.href = "article.html"
+  // }
   },  //success
   error : function(){
     console.log('error');
-  } //error
-}); //ajax
-}
+  } // error
+}); // ajax
+} // Click ends
